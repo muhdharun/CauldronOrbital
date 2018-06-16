8b90fe9648cde5df5e8a45afb2e96f174833fba4
Harun <harunabdul@yahoo.com.sg>
2018-06-16 12:21:09 +0800

    some more minor changes lol

---
+++ cauldron/Assets/Scripts/FBDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Unity.Editor;
+
+public class FBDatabase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Set a flag here indiciating that Firebase is ready to use by your
+                // application.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+        // Set this before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://cauldron-493c1.firebaseio.com/");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ cauldron/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public static bool SignedInYet = false;
+    public Text _LoginStatus1;
+    public Text _LoginStatus2;
+
+    public void Login()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+	
+    public void StartButtonPressed()
+    {
+        if (!SignedInYet)
+        {
+            _LoginStatus1.text = "Please login with acc or as guest first";
+        }
+        else
+        {
+            SceneManager.LoadSceneAsync("TypeOfMeal");
+        }
+
+    }
+
+	public void ContributeButtonPressed()
+    {
+        if (!SignedInYet)
+        {
+            _LoginStatus2.text = "Please login with acc or as guest first";
+        }
+    }
+
+}
