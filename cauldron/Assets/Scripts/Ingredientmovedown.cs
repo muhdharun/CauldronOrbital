e56a45a7141715eb036998ebcd4622859185a77c
ysy04136 <ysy04136@hotmail.com>
2018-06-24 13:47:45 +0800

    animations added

---
+++ cauldron/Assets/Scripts/Ingredientmovedown.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ingredientmovedown : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    // transform.Translate(Vector3.forward * Time.deltaTime);
+    //transform.position += transform.forward * Time.deltaTime;
+    /*transform.Translate(.05f, 0f, 0f);
+    transform.Rotate(0f, 0f, 1f);
+    transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);*/
+    public float fallSpeed = 1000.0f;
+    public float spinSpeed = 250.0f;
+    public float delay;
+
+    void Update()
+    {
+        StartCoroutine(ExecuteAfterTime(delay));
+        
+         /*if (gameObject.transform.position.y == 89.0f)
+          {
+              Destroy(gameObject);
+          }*/
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward, spinSpeed * Time.deltaTime);
+    }
+
+        /* void OnCollisionEnter(Collision col)
+         {
+             Destroy(gameObject);
+         } */
+        /*private void OnTriggerEnter(Collider other)
+        {
+            Destroy(gameObject);
+        } */
+    }
---
+++ cauldron/Assets/Scripts/Saltymotion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saltymotion : MonoBehaviour {
+   float timecounter = 0;
+   public float speed;
+    public float width;
+    public float height;
+    public float delay;
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        timecounter += Time.deltaTime * speed;
+
+        float x = Mathf.Cos(timecounter) * width;
+        float y = Mathf.Sin(timecounter) * height + 1000;
+        float z = 0;
+        transform.position = new Vector3(x, y, z);
+    }
+
+
+    // Use this for initialization
+
+
+    // Update is called once per frame
+    void Update () {
+        StartCoroutine(ExecuteAfterTime(delay));
+	}
+}
